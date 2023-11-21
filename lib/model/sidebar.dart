8f5ceef1c77bf00adedef2ce8967848eb67d2351
Ashik Mahmud <ashikmahmud137@gmail.com>
2023-11-21 19:32:56 +0600

    Sidebar screen updated

--- lib/model/sidebar.dart
+++ lib/model/sidebar.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SidebarItem {
@@ -15,14 +18,52 @@ class SidebarItem {
 
 var sidebarItems = [
   SidebarItem(
-      title: "Home",
-      background: const LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Color(0xFF00AEFE),
-            Color(0xFF0076FE),
-          ]),
-      icon: const Icon(Icons.home, color: Colors.white)
-  )
+    title: "Home",
+    background: const LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [
+        Color(0xFF00AEFF),
+        Color(0xFF0076FF),
+      ],
+    ),
+    icon: const Icon(
+      Icons.home,
+      color: Colors.white,
+    ),
+  ),
+  SidebarItem(
+    title: "Courses",
+    background: const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFFFA7d75), Color(0xFFC23D61)]),
+    icon: Icon(
+      Platform.isAndroid ? Icons.library_books : CupertinoIcons.book_solid,
+      color: Colors.white,
+    ),
+  ),
+  SidebarItem(
+    title: "Billing",
+    background: const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFFFAD64A), Color(0xFFEA880F)]),
+    icon: const Icon(
+      Icons.credit_card,
+      color: Colors.white,
+    ),
+  ),
+  SidebarItem(
+    title: "Settings",
+    background: const LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [Color(0xFF4E62CC), Color(0xFF202A78)],
+    ),
+    icon: Icon(
+      Platform.isAndroid ? Icons.settings : CupertinoIcons.settings_solid,
+      color: Colors.white,
+    ),
+  ),
 ];
